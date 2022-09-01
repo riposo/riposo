1892c5049ae78b845e8cfa3d95d0182d54eff932
Dimitrij Denissenko <dimitrij.denissenko@blacksquaremedia.com>
2022-09-01 12:36:20 +0100

    Upgrade to Go 1.19

--- internal/conn/postgres/permission/ext_test.go
+++ internal/conn/postgres/permission/ext_test.go
@@ -3,6 +3,8 @@ package permission
 // NumEntries is a test helper.
 func (tx *transaction) NumEntries() (int64, error) {
 	var cnt int64
+
+	//nolint:execinquery
 	err := tx.
 		QueryRowContext(tx.ctx, `
 			WITH totals AS (SELECT COUNT(1) AS cnt FROM permission_principals UNION ALL SELECT COUNT(1) AS cnt FROM permission_paths)
--- internal/conn/postgres/permission/statements.go
+++ internal/conn/postgres/permission/statements.go
@@ -1,9 +1,10 @@
 package permission
 
 // Placeholders:
-//   $1 - userID
-//   $2 - 'system.Authenticated'
-//   $3 - 'system.Everyone'
+//
+//	$1 - userID
+//	$2 - 'system.Authenticated'
+//	$3 - 'system.Everyone'
 const sqlGetUserPrincipals = `
 SELECT user_id, principal
 FROM permission_principals
@@ -11,8 +12,9 @@ WHERE user_id IN ($1, $2, $3)
 `
 
 // Placeholders:
-//   $1 - principal
-//   $2 - userIDs
+//
+//	$1 - principal
+//	$2 - userIDs
 const sqlRemoveUserPrincipal = `
 DELETE FROM permission_principals
 WHERE principal = $1
@@ -20,15 +22,17 @@ WHERE principal = $1
 `
 
 // Placeholders:
-//   $1 - principals
+//
+//	$1 - principals
 const sqlPurgeUserPrincipals = `
 DELETE FROM permission_principals
 WHERE principal = ANY($1)
 `
 
 // Placeholders:
-//   $1 - path
-//   $2 - perm
+//
+//	$1 - path
+//	$2 - perm
 const sqlGetACEPrincipals = `
 SELECT principal
 FROM permission_paths
@@ -37,8 +41,9 @@ WHERE path = $1
 `
 
 // Placeholders:
-//   $1 - path
-//   $2 - perm
+//
+//	$1 - path
+//	$2 - perm
 const sqlMatchACEPrincipals = `
 SELECT principal
 FROM permission_paths
@@ -47,9 +52,10 @@ WHERE path ~ $1
 `
 
 // Placeholders:
-//   $1 - path
-//   $2 - perm
-//   $3 - principal
+//
+//	$1 - path
+//	$2 - perm
+//	$3 - principal
 const sqlInsertACE = `
 INSERT INTO permission_paths (path, permission, principal)
 VALUES ($1, $2, $3)
@@ -57,9 +63,10 @@ ON CONFLICT (path, permission, principal) DO NOTHING
 `
 
 // Placeholders:
-//   $1 - path
-//   $2 - perm
-//   $3 - principal
+//
+//	$1 - path
+//	$2 - perm
+//	$3 - principal
 const sqlDeleteACE = `
 DELETE FROM permission_paths
 WHERE path = $1
@@ -68,7 +75,8 @@ WHERE path = $1
 `
 
 // Placeholders:
-//   $1 - path
+//
+//	$1 - path
 const sqlGetPerms = `
 SELECT permission, principal
 FROM permission_paths
